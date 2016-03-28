0bb97ff87ea3210694141dd8fc8813c612973ead
1312474 <1312474@student.hcmus.edu.vn>
2016-03-29 00:22:39 +0700

    Upload các file Javascript cho bàn cờ vua

---
+++ Javascript/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+			  $('#top-menu').prmenu({
+				  "fontsize": "14",
+					"height": "50",
+					"case": "capitalize",
+					"linkbgcolor": "#286090",
+					"linktextcolor": "#ffffff",
+					"linktextweight": "400",
+					"linktextfont": "sans-serif",
+					"hoverdark": false
+				});
+
+});
